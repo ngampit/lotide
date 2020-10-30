7feb5179287e0247f2059cf30745e67f3837c5e3
@mollysutthasilp <ngampitt@yahoo.com>
2020-10-30 01:12:42 +0000

    first commit

---
+++ takeUntil.js
@@ -0,0 +1,28 @@
+const takeUntil = function(array,callback) {
+    take = []
+    let i = 0;
+    while (!callback(array[i])) {
+        take.push(array[i]);
+        i++; 
+    }
+    return take; 
+ 
+}
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+
+
+
+//[ 1, 2, 5, 7, 2 ]
+//---
+//[ 'I\'ve', 'been', 'to', 'Hollywood' ]
