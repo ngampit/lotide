5e5beea191f3f5c15bd911a68d3b9f41e1d6cf71
@mollysutthasilp <ngampitt@yahoo.com>
2020-11-02 20:41:48 +0000

    refactored export module

--- middle.js
+++ middle.js
@@ -1,31 +1,4 @@
 // // FUNCTION IMPLEMENTATION
-// const assertArraysEqual = function(actual, expected) {
-//     if (eqArrays(actual,expected)) {
-//       console.log("Assertion Passed: " + actual + " === " + expected);
-//     } else  {
-//       console.log("Assertion Failed: " +  actual + " !== " + expected);
-//     }
-// };
-
-// const eqArrays = function(a,b) {
-//     if (a.length !== b.length){
-//         return false
-//     } else {
-//         for (let i = 0; i < a.length; i++){
-//           if (a[i] != b[i]){
-//              return false ; 
-//           }         
-//         } 
-//         return true;   
-//     }
-// }  
-
-//console.log(eqArrays([1,1,1],[1,1,1]));
-//console.log(assertEqual(eqArrays([1,1,1],[1,1,1]), true));
-
-// assertArraysEqual([1,1,1],[1,1,1]);
-// assertArraysEqual([1,1,1],[1,1,2]);
-
 
 const middle = function(array) {
     const a = [];
@@ -42,7 +15,6 @@ const middle = function(array) {
 }
 
 
-
-    
-console.log(middle([1,3,4,2,5,]));   
+module.exports = middle;  
+   
 
