e3e6be26baf2659616872448c9c09e36c5b5379f
@mollysutthasilp <ngampitt@yahoo.com>
2020-11-02 20:28:09 +0000

    refactored export module

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,6 @@
+const eqArrays = require(`./eqArrays`);
+
+
 // FUNCTION IMPLEMENTATION
 const assertArraysEqual = function(actual, expected) {
     if (eqArrays(actual,expected)) {
@@ -7,21 +10,7 @@ const assertArraysEqual = function(actual, expected) {
     }
 };
 
-const eqArrays = function(a,b) {
-    if (a.length !== b.length){
-        return false
-    } else {
-        for (let i = 0; i < a.length; i++){
-          if (a[i] != b[i]){
-             return false ; 
-          }         
-        } 
-        return true;   
-    }
-}  
+module.exports = assertArraysEqual;
 
-//console.log(eqArrays([1,1,1],[1,1,1]));
-//console.log(assertEqual(eqArrays([1,1,1],[1,1,1]), true));
 
-assertArraysEqual([1,1,1],[1,1,1]);
-assertArraysEqual([1,1,1],[1,1,2]);
+
